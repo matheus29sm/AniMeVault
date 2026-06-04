d64b77aefc43aabd1dc158f1ed9c9515b94c1286
matheus29sm <matheus05sm@gmail.com>
2026-06-04 17:10:40 -0300

    [init] Create AniMeVault project with Spring Boot and Maven Wrapper

---
+++ src/main/java/com/animevault/AniMeVaultApplication.java
@@ -0,0 +1,13 @@
+package com.animevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniMeVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniMeVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animevault/AniMeVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniMeVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
